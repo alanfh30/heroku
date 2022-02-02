e5dbea8f24491263a57908b564cb64f2f51113cc
Alan Flores <alanfh29132@gmail.com>
2022-02-01 20:05:38 -0600

    cambios india

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/demo-deploy'));
+app.use(express.static('./dist/demo-deploy'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/demo-deploy/index.html'));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/demo-deploy/'}),
+);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+
